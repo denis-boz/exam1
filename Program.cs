0541b568552f19e0c78e3118ce032f47a56d4bee
Denis <denis-boz@hotmail.com>
2023-07-09 20:03:09 +0300

    Создал массив array1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write($"Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array1 = new string[size];
+for (int i = 0; i < array1.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}: ");
+    array1[i] = Console.ReadLine()!;
+}
+Console.WriteLine($"Введенный массив: [{String.Join(", ", array1)}]");
